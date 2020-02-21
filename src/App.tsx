8e4fca5698666cf57787122c374da30dca2572a2
Spencer Loveridge <spencer.loveridge@togondo.com>
2020-02-21 09:45:47 +0000

    adding state-machine-cat

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {render as smcat} from 'state-machine-cat';
 
 function App() {
+
+  const lSVGInAString = smcat(
+      `
+            initial => backlog;
+            backlog => doing;
+            doing => test;
+        `,
+      {
+        outputType: "svg"
+      }
+  );
+  console.log(lSVGInAString);
   return (
     <div className="App">
       <header className="App-header">
